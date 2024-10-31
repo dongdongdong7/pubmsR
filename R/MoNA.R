02706508c409dc23918e84a066f1e6e5b25f2fc2
spd <384220729@qq.com>
2024-10-31 16:20:17 +0800

    Update load_monaMsTb function.

--- R/MoNA.R
+++ R/MoNA.R
@@ -2,15 +2,31 @@
 #' @description
 #' Load MassBank of North America.
 #'
+#' @param standard If TRUE, it will return a tibble with standard column names, which include accession, inchikey,
+#' formula, name, exactmass, smiles, inchi, precursorMz, adduct, polarity, collision_energy, instrument_type, instrument, predicted,
+#' mz, intensity.
+#'
 #' @return A tibble.
 #' @export
 #'
 #' @examples
 #' monaMsTb <- load_monaMsTb()
-load_monaMsTb <- function(){
+load_monaMsTb <- function(standard = TRUE){
   monaMsTb_path <- system.file("extdata", "monaMsTb.rds", package = "pubmsR")
   message("Load monaMsTb...")
   if(file.exists(monaMsTb_path)) monaMsTb <- tibble::tibble(readRDS(monaMsTb_path))
   else stop("Can not find monaMsTb, please redownload!")
+  if(standard){
+    monaMsTb$polarity <- unname(sapply(monaMsTb$polarity, function(x) {
+      if(is.na(x)) return(NA)
+      else if(x == 0) return("neg")
+      else if(x == 1) return("pos")
+      else if(x == -1) return("pos")
+      else return(NA)
+    }))
+    monaMsTb$predicted <- TRUE
+    monaMsTb <- monaMsTb %>%
+      dplyr::select(accession, inchikey, formula, name, exactmass, precursorMz, adduct, polarity, collision_energy, instrument_type, instrument, predicted, mz, intensity)
+  }
   return(monaMsTb)
 }
