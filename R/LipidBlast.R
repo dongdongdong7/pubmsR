ecdc1945d126a2721e79db87ce8b69446a98896f
spd <384220729@qq.com>
2024-10-31 16:44:07 +0800

    Update load_lipidblastMsTb function.

--- R/LipidBlast.R
+++ R/LipidBlast.R
@@ -1,6 +1,6 @@
-#' @title Load MoNA MS/MS Tibble
+#' @title Load LipidBlast MS/MS Tibble
 #' @description
-#' Load MassBank of North America.
+#' Load LipidBlast MS/MS Spectra.
 #'
 #' @param standard If TRUE, it will return a tibble with standard column names, which include accession, inchikey,
 #' formula, name, exactmass, smiles, inchi, precursorMz, adduct, polarity, collision_energy, instrument_type, instrument, predicted,
@@ -10,23 +10,23 @@
 #' @export
 #'
 #' @examples
-#' monaMsTb <- load_monaMsTb()
-load_monaMsTb <- function(standard = TRUE){
-  monaMsTb_path <- system.file("extdata", "monaMsTb.rds", package = "pubmsR")
-  message("Load monaMsTb...")
-  if(file.exists(monaMsTb_path)) monaMsTb <- tibble::tibble(readRDS(monaMsTb_path))
-  else stop("Can not find monaMsTb, please redownload!")
+#' lipidblastMsTb <- load_lipidblastMsTb()
+load_lipidblastMsTb <- function(standard = TRUE){
+  lipidblastMsTb_path <- system.file("extdata", "lipidblastMsTb.rds", package = "pubmsR")
+  message("Load lipidblastMsTb...")
+  if(file.exists(lipidblastMsTb_path)) lipidblastMsTb <- tibble::tibble(readRDS(lipidblastMsTb_path))
+  else stop("Can not find lipidblastMsTb, please redownload!")
   if(standard){
-    monaMsTb$polarity <- unname(sapply(monaMsTb$polarity, function(x) {
+    lipidblastMsTb$polarity <- unname(sapply(lipidblastMsTb$polarity, function(x) {
       if(is.na(x)) return(NA)
       else if(x == 0) return("neg")
       else if(x == 1) return("pos")
       else if(x == -1) return("pos")
       else return(NA)
     }))
-    monaMsTb$predicted <- NA
-    monaMsTb <- monaMsTb %>%
+    lipidblastMsTb$predicted <- FALSE
+    lipidblastMsTb <- lipidblastMsTb %>%
       dplyr::select(accession, inchikey, formula, name, exactmass, precursorMz, adduct, polarity, collision_energy, instrument_type, instrument, predicted, mz, intensity)
   }
-  return(monaMsTb)
+  return(lipidblastMsTb)
 }
